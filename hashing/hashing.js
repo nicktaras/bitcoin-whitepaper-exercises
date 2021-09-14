5686f1ba8ac38a4e07823529c44ef5dc168945c3
Nick Taras <nicktaras@hotmail.co.uk>
2021-09-14 23:00:30 +1000

    init commit

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -27,17 +27,52 @@ Blockchain.blocks.push({
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock({ data: line });
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+function createBlock({ data }) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length -1].hash, 
+		data, 
+		timeStamp: Date.now()
+	}
+	block.hash = blockHash(block);
+	Blockchain.blocks.push(block);
+	return block;
+}
 
-
-// **********************************
-
-function blockHash(bl) {
+function blockHash(block) {
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		JSON.stringify(block)
 	).digest("hex");
 }
+
+function verifyChain(Blockchain) {
+	var blockchainIsValid = true;
+	Blockchain.blocks.map((block, index) => {
+		if (index > 0 && blockchainIsValid) {
+			let blockCopy = JSON.stringify(block);
+			blockCopy = JSON.parse(blockCopy);
+			delete blockCopy.hash;
+			blockchainIsValid = (
+				block.data &&
+				block.prevHash &&
+				block.index >= 0 &&
+				block.hash === blockHash(blockCopy) &&
+				block.prevHash === Blockchain.blocks[index - 1].hash
+			) ? true : false;
+		}
+	})
+	return blockchainIsValid;
+}
+
+// Test to invalidate the blockchain
+// Blockchain.blocks[2].prevHash = 'a';
+
+console.log(
+	'blocks: ', Blockchain.blocks,
+	'verfied: ', verifyChain(Blockchain)
+);
